9904cb07af63eb598b7aa9b356512454390b7d9e
tnowls <tnowlan101@icloud.com>
2019-12-15 23:00:53 -0800

    questions

---
+++ assets/questions.js
@@ -0,0 +1,27 @@
+var questions = [
+    {
+        title: "What does HTML stand for?",
+        choices: ["hooligans that make lemonade", "hyper text makeup line", "hyper text markup language", "hyper text making language"],
+        answer: "hyper text markup language"
+    },
+    {
+        title: "Which of the following should you stay away from when naming a variable?",
+        choices: ["apples", "amazonprime", "hexigons", "class"],
+        answer: "class"
+    },
+    {
+        title: "Which of the following is a Javascript data type",
+        choices: ["boolean", "bass string", "poolbeans", "objectives"],
+        answer: "boolean"
+    },
+    {
+        title: "Where should you put the external Javascript syntax at in an HTMl document?",
+        choices: ["Before the Header", "After the html", "The end of the Body", "you dont you put in in the css"],
+        answer: "The end of the Body"
+    },
+    {
+        title: "What would you write if you wanted to put 'Thats all Folks' in an alert box?",
+        choices: ["msg('Thats all Folks');", "alertBox('Thats all Folks');", "msgBox('Thats all Folks');", "numalert('Thats all Folks');bers"],
+        answer: "alertBox('Thats all Folks');"
+    },
+];
